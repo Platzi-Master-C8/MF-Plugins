b0ada03c4fb2c0c1075351c63759a3bc9adac64e
jito <sergio.sergio.98@hotmail.com>
2021-12-06 17:53:09 -0300

    Add libraries to work in the charts and the loading UI state to the components

--- stories/containers/UserMain/UserMain.jsx
+++ stories/containers/UserMain/UserMain.jsx
@@ -1,18 +1,41 @@
 import React from 'react';
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 import './UserMain.css';
 import { Button } from '../../components/Button/Button';
 
-export const UserMain = ({ name, currentToken, getToken }) => {
+export const UserMain = ({ name, currentToken, getToken, loading, error }) => {
 
     return (
         <>
-            <section className="userMain">
-                <article>
-                    <h1>Welcome back, {name}</h1>
-                    <p>Your current token is: {currentToken}</p>
-                </article>
-                <Button name="Get New Token" onClick={getToken}/>
-            </section>
+            {
+                loading && !error ? 
+                <section className="userMain loading">
+                    <Skeleton containerClassName="loading-skeletonContainer" className="skeleton-item"/>
+                </section> :
+                null
+            }
+            {
+                error && !loading?
+                <section className="userMain error">
+                    <article>
+                        <h1>Welcome back, user</h1>
+                        <p>something is wrong</p>
+                    </article>
+                </section> :
+                null
+            }
+            {
+                !loading && !error ? 
+                <section className="userMain">
+                    <article>
+                        <h1>Welcome back, {name}</h1>
+                        <p>Your current token is: {currentToken}</p>
+                    </article>
+                    <Button name="Get New Token" onClick={getToken}/>
+                </section> : 
+                null
+            }
         </>
     )
 }
