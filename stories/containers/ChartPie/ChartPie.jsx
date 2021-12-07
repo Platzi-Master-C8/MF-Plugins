b2b75b78d19cce0b99e3d48d7522a6d475390d5d
jito <sergio.sergio.98@hotmail.com>
2021-12-07 19:26:05 -0300

    Add pie chart component

--- stories/containers/ChartPie/ChartPie.jsx
+++ stories/containers/ChartPie/ChartPie.jsx
@@ -1,49 +1,83 @@
-import { useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-// import { ChartPieItem } from '../../components/ChartPieItem/ChartPieItem';
+import { ChartPieItem } from '../../components/ChartPieItem/ChartPieItem';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
 import "../../globals.css";
 import './ChartPie.css';
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-const data = {
-    labels: [
-      'Red',
-      'Blue',
-      'Yellow'
-    ],
+
+
+export const ChartPie = ({ usedLanguages, loading }) => {
+
+  const colors = [
+    '#AE4EFF',
+    '#555BFF',
+    '#0AC533',
+    '#FF5353',
+    '#FBA618'
+  ]
+  const labels = usedLanguages.map( data => data.name )
+  const values = usedLanguages.map( data => data.value )
+  const chartData = {
+    labels: labels,
     datasets: [{
-      label: 'My First Dataset',
-      data: [300, 50, 100],
-      backgroundColor: [
-        'rgb(255, 99, 132)',
-        'rgb(54, 162, 235)',
-        'rgb(255, 205, 86)'
-      ],
+      data: values,
+      backgroundColor: colors,
       hoverOffset: 4
     }]
   };
 
-export const ChartPie = () => {
-   
-    return (
-        <>
-            <div className="pieChart-container">
-                <Pie data={data} />;
+  const chartOptions =  {
+    plugins: {
+        legend: {
+          display: false,
+        },
+    }
+  }
+
+  return (
+    <>
+      {
+        loading ?
+        <div className="pieChart loading">
+            <Skeleton containerClassName="loading-skeletonContainer" className="skeleton-item"/>
+        </div> :
+        null
+      }
+      { 
+        !loading ?        
+        <div className="pieChart">
+            <div className="chart-container">
+              <Pie options={chartOptions} data={chartData} />
             </div>
-
-
-        </>
-    )
+            <div className="chartItem-container">   
+              { 
+                usedLanguages ? 
+                usedLanguages.map((data, index) => (
+                  <ChartPieItem 
+                    key={data.name} 
+                    name={data.name} 
+                    percentage={data.value} 
+                    color={colors[index]}/>
+                )) : 
+                null
+              }       
+            </div>
+        </div> :
+        null
+        
+      }
+    </>
+  )
 }
 
 
 ChartPie.propTypes = {
-    userName: PropTypes.string,
-    currentToken: PropTypes.string,
+    usedLanguages: PropTypes.array,
     loading: PropTypes.bool,
-    errorInGetLogin: PropTypes.bool,
-    getToken: PropTypes.func,
 };
   
--- stories/containers/ChartPie/ChartPie.stories.jsx
+++ stories/containers/ChartPie/ChartPie.stories.jsx
@@ -14,5 +14,26 @@ const Template = (args) => <ChartPie {...args} />
 export const Default = Template.bind({})
 Default.args = {
     loading: false,
-
+    usedLanguages: [
+        {
+          name: "JS",
+          value: 12.5,
+        },
+        {
+          name: "Rust",
+          value: 25,
+        },
+        {
+          name: "php",
+          value: 25,
+        },
+        {
+          name: "Java",
+          value: 12.5,
+        },
+        {
+          name: "Python",
+          value: 25,
+        },
+    ]
 }
