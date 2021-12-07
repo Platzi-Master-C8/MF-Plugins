3f16ac3cc243bf950d1c59f41bcb1ed1c78bf577
kbzaso <alejandro.sapa@gmail.com>
2021-12-07 17:10:26 -0300

    half doughtnut layout

---
+++ stories/containers/ChartHalfDoughtnut/ChartHalfDoughtnut.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+import '../../globals.css';
+import './ChartHalfDoughtnut.css';
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export const data = {
+  labels: ['Today', 'Daily Average'],
+  datasets: [
+    {
+      label: 'time develop',
+      data: [75, 25],
+      backgroundColor: ['#AE4EFF', '#555BFF'],
+      borderWidth: 0,
+    },
+  ],
+};
+
+export const options = {
+  rotation: -90,
+  circumference: 180,
+  plugins: {
+    legend: {
+      display: false,
+    },
+    tooltip: {
+      enabled: false,
+    },
+  },
+};
+
+export const ChartHalfDoughtnut = ({}) => {
+  return (
+    <>
+      <div className="donnutChart-container">
+        <div class="chart-title">
+          <h2>
+            Thu Nov 12th <span>3 hrs 00 mins</span>
+          </h2>
+        </div>
+        <div className="donnutChart">
+          <Doughnut data={data} options={options} />
+        </div>
+        <p class="percent">86%</p>
+        <p class="average">
+          Daily Average: <span>3 hrs 33 mins</span>
+        </p>
+      </div>
+    </>
+  );
+};
+
+// UserMain.propTypes = {
+//   userName: PropTypes.string,
+//   currentToken: PropTypes.string,
+//   loading: PropTypes.bool,
+//   errorInGetLogin: PropTypes.bool,
+//   getToken: PropTypes.func,
+// };
