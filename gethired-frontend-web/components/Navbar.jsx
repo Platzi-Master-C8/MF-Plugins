ae216f2caff6419aaa39e78021ce50b170df9f55
Irving Juarez <irvingjuarez274@gmail.com>
2021-12-09 16:45:08 -0600

    The layout is done

--- gethired-frontend-web/components/Navbar.jsx
+++ gethired-frontend-web/components/Navbar.jsx
@@ -3,12 +3,14 @@ import Image from "next/image";
 
 const Navbar = () => (
   <header>
-    <Link href="/">
-      <a>
-        <Image width={99} height={24} src="/images/LogoPlataforma.png" alt="logo"/>
-      </a>
-    </Link>
-    <nav></nav>
+    <div>
+      <Link href="/">
+        <a>
+          <Image width={135.6} height={36} src="/images/LogoPlataforma.png" alt="logo"/>
+        </a>
+      </Link>
+      <nav></nav>
+    </div>
   </header>
 )
 
