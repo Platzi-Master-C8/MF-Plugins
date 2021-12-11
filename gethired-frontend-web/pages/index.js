fb0dadfbc94266bb6db7b68a33e0786d1d49035d
Irving Juarez <irvingjuarez274@gmail.com>
2021-12-11 08:30:46 -0600

    Second page already done

--- gethired-frontend-web/pages/index.js
+++ gethired-frontend-web/pages/index.js
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link"
 import Path from "../components/Path"
 import TechImg from "../public/technology.png"
 import ButtonCTA from "../components/ButtonCTA/ButtonCTA"
@@ -8,7 +9,7 @@ export default function Home() {
   return (
     <main className="main">
       <section className="main__content">
-        <h2>What is GH-Code Tracking?</h2>
+        <h2 className="main__title">What is GH-Code Tracking?</h2>
         <div>
           <p>
           GH-Code Tracking is an extension for your VSCode text editor. It was built focusing on tech professional looking for mesuring their time and performance while writing code. With this extension you can track, share and visualize information about your time using VSCode only a click away. Show off your stats with recruiters and partners now!
@@ -16,7 +17,12 @@ export default function Home() {
 
           <Image src={TechImg} />
         </div>
-        <ButtonCTA name="Get the extension in VSCode" />
+
+        <Link href="/steps/two">
+          <a>
+            <ButtonCTA name="Get the extension in VSCode" />
+          </a>
+        </Link>
       </section>
 
       <Path active={1}/>
