50018730698a26d9decdd859d821f973a45cc6f8
Irving Juarez <irvingjuarez274@gmail.com>
2021-12-13 17:04:56 -0600

    The third page was already done into mobile and desktop

--- gethired-frontend-web/pages/steps/three.jsx
+++ gethired-frontend-web/pages/steps/three.jsx
@@ -7,14 +7,14 @@ import ButtonCTA from "../../components/ButtonCTA/ButtonCTA"
 const Step3 = () => (
 
   <main className="main">
-    <section className="main__content">
+    <section className="main__content three">
       <h2 className="main__title">Paste your token in your VSCode extension</h2>
       <div>
         <p>
           Paste your current token in your VSCode extension to almost finish the setup of your GH-Code Tracker.
         </p>
 
-        <Image width={400} height={330} src={TechImg} />
+        <Image width={400} height={250} src={TechImg} />
       </div>
 
       <Link href="/steps/four">
