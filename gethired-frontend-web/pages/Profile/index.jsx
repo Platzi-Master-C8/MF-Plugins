6b60260047d36cfb0dfd4efb21a76c877e6225a8
Irving Juárez <irvingjuarez274@gmail.com>
2022-02-15 19:07:55 -0600

    The Profile page has been done successfully

--- gethired-frontend-web/pages/Profile/index.jsx
+++ gethired-frontend-web/pages/Profile/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 import { Button } from "../../components/Button/Button";
 import Image from 'next/image';
 import visible from "../../public/visible.svg";
@@ -11,36 +12,54 @@ export default function Profile({ state }){
   if(state){
     return(
       <section className="wrapper Profile">
-  
-        <section className="Profile__section">
-          <h2>#Profile</h2>
-          <label>Profile Picture</label>
-          <Button name="Add a picture"/>
-  
-          <div>
-            <label htmlFor="firstName">First Name</label>
-            <input type="text" id="firstName" defaultValue={state.name} />
-  
-            <label htmlFor="lastName">Last Name</label>
-            <input type="text" id="lastName" />
-          </div>
-  
-          <label htmlFor="">Email</label>
-          <input type="email" defaultValue={state.email} />
-  
+        <section className="Profile__container">
+          <h2>Settings</h2>
+
+          <ul>
+            <li>
+              <Link href="#Profile">
+                <a>Profile</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="#Security">
+                <a>Security</a>
+              </Link>
+            </li>
+          </ul>
         </section>
   
-        <section className="Profile__section">
-          <h2>#Security</h2>
-          <label htmlFor="">Current Token</label>
-          <div className="Profile__token-container">
-            <input type={isVisible ? "text" : "password"} readOnly defaultValue={state.token} />
-            <button onClick={handleClick}>
-              <Image src={isVisible ? visible : hidden} alt="" />
-            </button>
-          </div>
-  
-          <Button name="Get new token"/>       
+        <section className="Profile__container">
+          <section className="Profile__section" id="Profile">
+            <h2>#Profile</h2>
+            <label>Profile Picture</label>
+            <Button name="Add a picture"/>
+    
+            <div>
+              <label htmlFor="firstName">First Name</label>
+              <input type="text" id="firstName" defaultValue={state.name} />
+    
+              <label htmlFor="lastName">Last Name</label>
+              <input type="text" id="lastName" />
+            </div>
+    
+            <label htmlFor="">Email</label>
+            <input type="email" defaultValue={state.email} />
+    
+          </section>
+    
+          <section className="Profile__section" id="Security">
+            <h2>#Security</h2>
+            <label htmlFor="">Current Token</label>
+            <div className="Profile__token-container">
+              <input type={isVisible ? "text" : "password"} readOnly defaultValue={state.token} />
+              <button onClick={handleClick}>
+                <Image src={isVisible ? visible : hidden} alt="" />
+              </button>
+            </div>
+    
+            <Button name="Get new token"/>       
+          </section>
         </section>
       </section>
     )
